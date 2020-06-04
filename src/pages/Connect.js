305a9653752a5d27bacc75405b3a01e8f9c7cb61
Jean Peel <jeanine.peel@gmail.com>
2020-06-03 20:24:08 -0600

    fixed css issue

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 // import axios from 'axios';
 import "./style.css";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 class Connect extends Component {
     // state = {}
@@ -14,10 +15,36 @@ class Connect extends Component {
         return (
             <div className="page_body">
                 <NavBar />
+                <div class='top_spacer'></div>
                 <h1>Connect</h1>
                 <div className='text_style'>
-                    <h6>Prow scuttle parrel  <a href="#" class="link"> Sail ho shrouds </a> spirits boom</h6>
+                    <h6>Prow scuttle parrel  <a href="#" class="link"> Sail ho shrouds </a>spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.</h6>
+
+                    <h6>Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.</h6>
+
+                    <h6>Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas boatswain schooner gaff booty Jack Tar transom spirits.</h6>
+
+                    <h6>Prow scuttle parrel  <a href="#" class="link"> Sail ho shrouds </a>spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.</h6>
+
+                    <h6>Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.</h6>
+
+                    <h6>Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas boatswain schooner gaff booty Jack Tar transom spirits.</h6>
+
+                    <h6>Prow scuttle parrel  <a href="#" class="link"> Sail ho shrouds </a>spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.</h6>
+
+                    <h6>Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.</h6>
+
+                    <h6>Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas boatswain schooner gaff booty Jack Tar transom spirits.</h6>
+
+                    <h6>Prow scuttle parrel  <a href="#" class="link"> Sail ho shrouds </a>spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.</h6>
+
+                    <h6>Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.</h6>
+
+                    <h6>Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas boatswain schooner gaff booty Jack Tar transom spirits.</h6>
+
                 </div>
+                <div class='bottom_spacer'></div>
+                <Footer />
             </div>
         );
     }
