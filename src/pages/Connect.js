1c60714c55d3e8588f09790531bbd11458ee8071
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-07 01:04:46 -0600

    css finalized

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -4,6 +4,7 @@ import "./style.css";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import goodCoffee2 from "../components/images/goodCoffee2.jpg"
+import profilepic from "../components/images/profilepic.jpg"
 
 class Connect extends Component {
     // state = {}
@@ -18,7 +19,7 @@ class Connect extends Component {
                 <NavBar />
                 <div className='top_spacer'></div>
                 <div className='text_style'>
-                    <div className='header_div'>
+                <div className='header_div'>
                         <h1>Connect with<br />Jean Peel</h1>
                     </div>
                     <div className='text_style'>
@@ -28,30 +29,33 @@ class Connect extends Component {
                         <h6><a href="https://gitlab.com/jean.peel.246" className="link_style">GitLab</a></h6>
                         {/* <h6>Gravatar<a href="https://en.gravatar.com/jeanpeel" className="link_style"></a></h6> */}
                     </div>
-                    <div className='padding_top'>
-                    <div className='header_div'>
-                        <h1>Email Jean Peel</h1>
-                    </div>
-                        <form>
-                            <div className='form_text'>Name</div>
-                            <input type="text" name="Name" className="contact-form" id="name" value="Enter your name here..." /><br />
-                            <div className='form_text'>Email</div>
-                            <input type="text" name="Email" className="contact-form" id="email" value="Enter your email here..." /><br />
-                            <div className='form_text'>Message</div>
-                            <textarea name="Message" rows="10" cols="30" className="contact-form" value="Enter your email message here..." >
-                            </textarea><br />
-                            <button type="button" className="btn btn-info">Submit</button>
-                        </form>
-                    </div>
-                    <div>
-                    <div className='bottom_spacer'></div>
-                    <div className='image_div2'><img src={goodCoffee2} alt="Candid Photo" className="blog_pic" /></div>
+                    <div className='header_div padding_top'>
+                            <h1>Email Jean Peel</h1>
+                        </div>
+                        
+                    <div className='image_div2'><img src={profilepic} alt="Candid Photo" className="blog_pic" /></div>
+                    <div className='form_div'>
+
+<form>
+    <div className='form_text'>Name</div>
+    <input type="text" name="Name" className="contact-form" id="name" value="Enter your name here..." /><br />
+    <div className='form_text'>Email</div>
+    <input type="text" name="Email" className="contact-form" id="email" value="Enter your email here..." /><br />
+    <div className='form_text'>Message</div>
+    <textarea name="Message" rows="10" cols="30" className="contact-form" value="Enter your email message here..." >
+    </textarea><br />
+    <button type="button" className="btn btn-info">Submit</button>
+</form>
+</div>
                             <br />
-                            <div id="about-me" className="d-flex justify-content-center"><a href="#" ></a></div>
-                            <div id="about-me" className="d-flex justify-content-center"></div><br />
-                        <div className="d-flex flex-row-reverse more"><a href="#" ></a></div>
-                        <br />
-                    </div>
+                            <div id="about-me" className="d-flex justify-content-center"></div>
+                   
+                        <div className='blog_text'>
+                        <p className='full_width'>
+                            For me a good day starts with coffee.  A better day starts after a full nights sleep.  I have been working from home on and off for about 20 years.  I just finished managing two major home remodels that were profitable.
+                           </p>
+                        </div>
+
                     <div className='bottom_spacer'></div>
                 </div>
                 <Footer />
