2c8c7b57d392fa52003883d0fdca98e0095f4bf6
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-06 19:13:09 -0600

    added contact info

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -18,10 +18,29 @@ class Connect extends Component {
                 <div className='top_spacer'></div>
                 <div className='text_style'>
                     <div className='header_div'>
-                        <h1>Connect</h1>
-                        </div>
+                        <h1>Connect with<br />Jean Peel</h1>
+                    </div>
                     <div className='text_style'>
-                        <h6>Prow scuttle parrel  <a href="#" className="link_style"> Sail ho shrouds </a> spirits boom</h6>
+                        <h6><a href="https://www.linkedin.com/in/jean-peel-b52234198/" className="link_style">LinkedIn</a></h6>
+                        <h6><a href="https://github.com/JeanPeel" className="link_style">GitHub</a></h6>
+                        <h6><a href="https://twitter.com/peel_jean" className="link_style">Twitter</a></h6>
+                        <h6><a href="https://gitlab.com/jean.peel.246" className="link_style">GitLab</a></h6>
+                        {/* <h6>Gravatar<a href="https://en.gravatar.com/jeanpeel" className="link_style"></a></h6> */}
+                    </div>
+                    <div>
+                    <div className='header_div'>
+                        <h1>Email Jean Peel</h1>
+                    </div>
+                        <form>
+                            Name<br />
+                            <input type="text" name="Name" class="contact-form" id="name" /><br />
+                                    Email<br />
+                            <input type="text" name="Email" class="contact-form" id="email" /><br />
+                                            Message<br />
+                            <textarea name="message" rows="10" cols="30" class="contact-form" >
+                            </textarea><br />
+                            <button type="button" class="btn btn-info">Submit</button>
+                        </form>
                     </div>
                     <div className='bottom_spacer'></div>
                 </div>
