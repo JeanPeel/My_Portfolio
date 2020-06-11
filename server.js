544ca3ea2a462b068858870266ce0ef704932b91
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-10 22:28:28 -0600

    reverted netlify efforts

--- server.js
+++
@@ -1,9 +0,0 @@
-var express   =     require("express");
-var app       =     express();
-
-app.use("./src/pages/Home.js",express.static("home"));
-app.use("./src/pages/About.js",express.static("about"));
-app.use("./src/pages/Blog.js",express.static("blog"));
-app.use("./src/pages/Connect.js",express.static("connect"));
-app.use("./src/pages/Portfolio.js",express.static("portfolio"));
-app.use("./src/pages/Resume.js",express.static("resume"));
